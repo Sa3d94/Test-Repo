ed1a34d15ffb198553fd4b55a6f2af29654bccca
ziadelkersh <ziadelersh@gmail.com>
2025-11-04 03:32:18 +0200

    Publishing playwright repo

---
+++ tests/actions.spec.ts
@@ -0,0 +1,165 @@
+import { test, expect } from '@playwright/test';
+
+test('textbox', async({page})=> {
+  await page.goto('https://the-internet.herokuapp.com/login');
+  await page.locator('[id="username"]').fill('tomsmith');
+  await page.locator('[id="password"]').pressSequentially('SuperSecretPassword!',{delay:200});
+  await page.locator('[id="password"]').press('Enter');
+
+  await page.close();
+})
+
+test('click', async({page})=> {
+    await page.goto('https://play1.automationcamp.ir/mouse_events.html');
+    await page.locator('[id="click_area"]').click();
+    await expect (page.locator('[id="click_type"]')).toHaveText('Click')
+    await page.locator('[id="click_area"]').dblclick();
+    await expect (page.locator('[id="click_type"]')).toHaveText('Double-Click')
+    await page.locator('[id="click_area"]').click({button : 'right'});
+    await expect (page.locator('[id="click_type"]')).toHaveText('Right-Click')
+    await page.close();
+  })
+
+  test('radio', async({page})=> {
+    await page.goto('http://test.rubywatir.com/radios.php');
+    await page.locator('[class="radioclass"]').check();
+    await expect (page.locator('[id="radioId"]')).not.toBeChecked();
+    await page.locator('[id="radioId"]').check();
+    await expect (page.locator('[id="radioId"]')).toBeChecked();
+    await page.close();
+  })
+
+
+  test('checkbox', async({page})=> {
+    await page.goto('https://the-internet.herokuapp.com/checkboxes');
+    await page.locator('(//input[@type="checkbox"])[1]').uncheck();
+    await expect (page.locator('(//input[@type="checkbox"])[1]')).not.toBeChecked();
+    await page.locator('(//input[@type="checkbox"])[1]').check();
+    await expect (page.locator('(//input[@type="checkbox"])[1]').isChecked).toBeTruthy();
+  
+    await page.close();
+  })
+
+  test('dropdown', async({page})=> {
+    await page.goto('https://the-internet.herokuapp.com/dropdown');
+    await page.selectOption('[id="dropdown"]' , {
+        value:"1"
+    });
+    await page.pause();
+    await page.selectOption('[id="dropdown"]' , {
+        label:"Option 2"
+    });
+    await page.pause();
+    await page.selectOption('[id="dropdown"]' , {
+        index:2
+    });
+    await page.pause();
+  
+    await page.close();
+  })
+
+  test('multiselect', async({page})=> {
+    await page.goto('https://www.lambdatest.com/selenium-playground/select-dropdown-demo');
+    await page.selectOption('[id="multi-select"]' , [
+        {value:"California"},
+        {value:"Florida"},
+        {value:"New Jersey"}
+    ]);
+    
+    await page.pause();
+    await page.close();
+  })
+  test('dynamic', async({page})=> {
+    await page.goto('https://demo.automationtesting.in/Register.html');
+    await page.locator('//span[@role="combobox"]').click();
+    await page.locator('//li[text()="India"]').click();
+    await page.pause();
+    await page.close();
+  })
+
+  test('alert', async({page})=> {
+    await page.goto('https://the-internet.herokuapp.com/javascript_alerts');
+    
+    page.on("dialog", async(alert)=>{
+      const alertMessage = alert.message();
+      expect (alertMessage).toEqual('I am a JS Alert');
+      await alert.accept();
+      await expect (page.locator('[id="result"]')).toHaveText('You successfully clicked an alert');
+    })
+    await page.locator('//button[@onclick="jsAlert()"]').click();
+    
+   // await page.pause();
+    await page.close();
+  })
+
+  test('confirmation ok', async({page})=> {
+    await page.goto('https://the-internet.herokuapp.com/javascript_alerts');
+    
+    page.on("dialog", async(alert)=>{
+      const alertMessage = alert.message();
+      expect (alertMessage).toEqual('I am a JS Confirm');
+      await alert.accept();
+      await expect (page.locator('[id="result"]')).toHaveText('You clicked: Ok');
+    })
+    await page.locator('[onclick="jsConfirm()"]').click();
+    
+   // await page.pause();
+    await page.close();
+  })
+
+  test('cancel', async({page})=> {
+    await page.goto('https://the-internet.herokuapp.com/javascript_alerts');
+    
+    page.on("dialog", async(alert)=>{
+      const alertMessage = alert.message();
+      expect (alertMessage).toEqual('I am a JS Confirm');
+      await alert.dismiss();
+      await expect (page.locator('[id="result"]')).toHaveText('You clicked: Cancel');
+    })
+    await page.locator('[onclick="jsConfirm()"]').click();
+    
+   // await page.pause();
+    await page.close();
+  })
+  test.only('okpromptalert', async({page})=> {
+    await page.goto('https://the-internet.herokuapp.com/javascript_alerts');
+    
+    page.on("dialog", async(alert)=>{
+      const alertMessage = alert.message();
+      expect (alertMessage).toEqual('I am a JS prompt');
+      await alert.accept("Ziad");
+      await expect (page.locator('[id="result"]')).toHaveText('You entered: Ziad');
+    })
+    await page.locator('[onclick="jsPrompt()"]').click();
+    
+   // await page.pause();
+    await page.close();
+  })
+  test.only('cancelpromptalert', async({page})=> {
+    await page.goto('https://the-internet.herokuapp.com/javascript_alerts');
+    
+    page.on("dialog", async(alert)=>{
+      const alertMessage = alert.message();
+      expect (alertMessage).toEqual('I am a JS prompt');
+      await alert.dismiss();
+      await expect (page.locator('[id="result"]')).toHaveText('You entered: null');
+    })
+    await page.locator('[onclick="jsPrompt()"]').click();
+    
+   // await page.pause();
+    await page.close();
+  })
+
+  test('frames', async({page})=> {
+    await page.goto('https://the-internet.herokuapp.com/nested_frames');
+    let framescount = page.frames().length;
+    console.log('The frame count is '  + framescount);
+    let bottomframe = page.frameLocator('[src="/frame_bottom"]').locator('//body[contains(text(),"BOTTOM")]');
+    await expect (bottomframe).toHaveText('BOTTOM');
+    let topframe = page.frame('frame-top')
+    let topframechilds = topframe?.childFrames() || [];
+    let middleframe= topframechilds[1];
+    await expect (middleframe.locator('[id="content"]')).toHaveText('MIDDLE');
+   // await page.pause();
+    await page.close();
+  })
---
+++ tests/herokuApp.spec.ts
@@ -0,0 +1,17 @@
+import { test, expect } from '@playwright/test';
+
+test('Heroku', async ({ page }) => {
+  await page.goto('https://the-internet.herokuapp.com/');
+  await page.getByRole('heading', { name: 'Welcome to the-internet1' }).click();
+  await expect(page.locator('h1')).toContainText('Welcome to the-internet');
+  await page.getByRole('link', { name: 'Form Authentication' }).click();
+  await expect(page.getByRole('textbox', { name: 'Username' })).toBeVisible();
+  await page.getByRole('textbox', { name: 'Username' }).click();
+  await page.getByRole('textbox', { name: 'Username' }).fill('tomsmith');
+  await page.getByRole('textbox', { name: 'Password' }).click();
+  await page.getByRole('textbox', { name: 'Password' }).fill('SuperSecretPassword!');
+  await expect(page.getByRole('button', { name: ' Login' })).toBeVisible();
+  await page.getByRole('button', { name: ' Login' }).click();
+  await expect(page.getByRole('link', { name: 'Logout' })).toBeVisible();
+  await page.getByRole('link', { name: 'Logout' }).click();
+});
